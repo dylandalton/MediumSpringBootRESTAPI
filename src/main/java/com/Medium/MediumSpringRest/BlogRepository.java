7fe209cbd576dd1cfc58be7030f5a6b69f6c6c6d
dylandalton <dylandalton.za@gmail.com>
2020-05-25 23:55:37 +0100

    Finished Project

---
+++ src/main/java/com/Medium/MediumSpringRest/BlogRepository.java
@@ -0,0 +1,13 @@
+package com.Medium.MediumSpringRest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Integer> {
+
+    List<Blog> findByTitleContainingOOrContentContaining(String title, String content);
+
+}
